2d3ca37653129a5e98d584bb922e04b2f9fbf86f
Sean Jaeger <jaegers@sheridancollege.ca>
2019-04-11 23:33:31 -0400

    Merge origin/georgy_dhanjal into Sean
    Conflicts:
    	.gitignore
    	nbproject/project.properties
    	src/ca/sheridancollege/project/Card.java
    	src/ca/sheridancollege/project/GameController.java
    	src/ca/sheridancollege/project/GoFish.java
    	src/ca/sheridancollege/project/GroupOfCards.java
    	src/ca/sheridancollege/project/Player.java
    	src/ca/sheridancollege/project/Score.java

--- src/ca/sheridancollege/project/GoFishView.java
+++ src/ca/sheridancollege/project/GoFishView.java
@@ -12,63 +12,77 @@ import java.util.Scanner;
  *
  * @author Sean
  */
-public class GoFishView{
-    
+public class GoFishView {
+
+    Player[] players;
+
     private final GoFishController controller;
-    
-    public GoFishView(){
+
+    public GoFishView() {
         controller = new GoFishController();
     }
-    
-    public void gameInit(){
+
+    public void gameInit() {
         //Create scanner object
         Scanner input = new Scanner(System.in);
-        
+
         // Initialize, generate, and shuffle deck
         Deck deck = new Deck(52);
         deck.generateCards();
         deck.shuffle();
-        
+
         // Declare total amount of players
         System.out.println("How many players?: "
                 + "\n2 PLAYERS"
                 + "\n3 PLAYERS"
                 + "\n4 PLAYERS");
         int totalPlayers = input.nextInt();
-        
+
         // Set important information to create game
-        controller.createGame("Go Fish", totalPlayers);   
+        controller.createGame("Go Fish", totalPlayers);
         controller.setHandsize();
-        
+
         // Player creation
         for (int i = 0; i < totalPlayers; i++) {
-            System.out.printf("Player %d, please enter your name: %n", i+1);
+            System.out.printf("Player %d, please enter your name: %n", i + 1);
             String playerName = input.next();
-            
+
             controller.addPlayers(i, playerName);
-            
+
         }
-        
-        Player[] players = controller.
+
+//        Player[] players = controller.
         int handSize = controller.getThisHandSize();
-        
+
         deck.deal(players, handSize);
-        
-        
+
 //        controller.displayPlayers();
-        
     }
-    
-    public void playerTurn(){
-        Player winner = controller.get
-        while()
+
+    public void playerTurn() {
+        Scanner input = new Scanner(System.in);
+        Player winner = controller.thisWinner();
+        while (winner == null) {
+
+            for (Player player : players) {
+                System.out.println("Who do you want to get a card from?");
+                for (int i = 0; i < players.length; i++) {
+                    System.out.println((i + 1) + ". " + players[i].getPlayerName());
+                    int userChoice = input.nextInt();
+                    System.out.println("Which card?");
+                    String selectedCard = input.nextLine();
+                    player.askPlayer(player[userChoice], askingCard, deck);
+                }
+            }
+
+        }
         controller.displayPlayer(0);
     }
-    
-    public void gameEnd(){
-        
+
+    public void gameEnd() {
+
     }
-    
+
     public static void main(String[] args) {
         GoFishView goFish = new GoFishView();
         goFish.gameInit();
